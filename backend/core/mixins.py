5544f559f8c94592c51ef8962f39ede30306c171
arjunhm <hm.arjun97@gmail.com>
2024-12-04 18:12:00 -0600

    add: pagination mixin

--- backend/core/mixins.py
+++ backend/core/mixins.py
@@ -1,13 +1,3 @@
-# custom pagination class
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
-
-
-class StandardPagination(LimitOffsetPagination):
-    default_limit = 10
-    limit_query_param = "per_page"
-    offset_query_param = "page"
-
-
 class StandardPaginationMixin(object):
     @property
     def paginator(self):
